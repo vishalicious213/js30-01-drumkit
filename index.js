7b99f4092f9c3b6596a6f01a0034c4b084e5a21d
vishalicious213 <vishalicious213@gmail.com>
2023-01-06 12:42:50 -0800

    pressing a letter key in the middle row add a .playing class with a transition of .07 seconds that styles the pressed key's button onscreen

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ console.log("drum kit")
 document.addEventListener("keydown", (e) => {
     // console.log(e.key)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
     if (!audio) return
     audio.currentTime = 0 // on repeated keypress, rewind sound so it plays from start instead of waiting for previous play to finish
     audio.play()
+
+    key.classList.add("playing")
+
     // console.log(audio)
+    // console.log(key)
 })
