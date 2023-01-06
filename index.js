7ab6d4c2d51df360ecc39e9845dc35ee1cc5e896
vishalicious213 <vishalicious213@gmail.com>
2023-01-06 12:20:40 -0800

    added eventListened that looks for keydown and console.logs 'audio' element associated with its keyCode. note: keyCode has been deprecated. might have to alter this later

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("drum kit")
+
+document.addEventListener("keydown", (e) => {
+    // console.log(e.key)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if (!audio) return
+    console.log(audio)
+})
