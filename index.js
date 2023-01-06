36085d9248c80b4d5064834567f63b46efb184e4
vishalicious213 <vishalicious213@gmail.com>
2023-01-06 12:31:52 -0800

    pressing a letter key in the middle row plays an associated wav file

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ document.addEventListener("keydown", (e) => {
     // console.log(e.key)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     if (!audio) return
-    console.log(audio)
+    audio.play()
+    // console.log(audio)
 })
