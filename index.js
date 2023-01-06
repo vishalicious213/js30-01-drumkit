d75cf347f4f44b9ee794a94df1cce3bfc2f3814d
vishalicious213 <vishalicious213@gmail.com>
2023-01-06 12:35:27 -0800

    pressing a key to play a sound restarts the sound, so prior running instance of that sound doesn't have to complete before new instance starts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.addEventListener("keydown", (e) => {
     // console.log(e.key)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     if (!audio) return
+    audio.currentTime = 0 // on repeated keypress, rewind sound so it plays from start instead of waiting for previous play to finish
     audio.play()
     // console.log(audio)
 })
